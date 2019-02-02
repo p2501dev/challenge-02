1fe1aac2ddab3707badc04c8ce30c8bad2576b06
hwolf <hannes.wolf@iti-software.de>
2019-02-02 15:51:25 +0100

    generated components and service

--- apps/challenge/src/app/app.module.ts
+++ apps/challenge/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TableViewComponent } from './components/table-view/table-view.component';
+import { GridComponent } from './components/grid/grid.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TableViewComponent, GridComponent, DetailsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/challenge/src/app/components/table-view/table-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'chll2-table-view',
+  templateUrl: './table-view.component.html',
+  styleUrls: ['./table-view.component.scss'],
+})
+export class TableViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
