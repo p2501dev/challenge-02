e304d60517b6f88f7f8e83fa0b1d30f82b54df93
hwolf <hannes.wolf@iti-software.de>
2019-02-02 15:38:30 +0100

    added git hooks / linting

--- apps/challenge/src/app/app.component.ts
+++ apps/challenge/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "chll2-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'chll2-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
